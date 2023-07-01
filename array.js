57d8dd06eca65e7041393564dfa28bc310a74dd1
Ahammed Faisal <137277285+ahammedfaisal05@users.noreply.github.com>
2023-07-01 20:14:11 +0600

    commit array file

---
+++ array.js
@@ -0,0 +1,5 @@
+let letter = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+letter.forEach((item, index) => {
+    console.log(`${index + 1 }.${item}`);
+})
