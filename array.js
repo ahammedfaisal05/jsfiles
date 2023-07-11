a49df3a5a5450744ea79fea8a65fbcc8bdcff2bf
ahammedfaisal05 <faisalahammed05@gmail.com>
2023-07-11 13:58:04 +0600

    learn improve array

---
+++ array.js
@@ -0,0 +1,54 @@
+// let letter = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+// letter.forEach((item, index) => {
+//     console.log(`${index + 1 }.${item}`);
+// })
+
+// let month = ['january','fabruary','march','april','may','jun','july','august','september','october','nobember','december'];
+
+// month.map((trrr,item) => {
+//     if(trrr === 'july'){
+//         console.log(`7th month is ${trrr}`);
+//     }
+// });
+
+// month.forEach((trrr,item) => {
+//     if(trrr !== 'july'){
+//         console.log(`${item} month is ${trrr} `);
+//     }
+// });
+
+// console.log(month.length);
+
+// for(let i = 0; i< month.length; i++){ //ata ke bole iteration
+//     console.log(month[i]);
+// }
+
+// let food = ['alo','potol','kumra','lao','derosh','morich',];
+// let fruits = ['mango','apple','banana','cherry','pineapple','blackberry'];
+// let flowers = ['beli','golap','hasnehena','gondhoraj','sioli','surjomukhi'];
+
+// let all_arr = food.concat(fruits).concat(flowers);//old js system.
+// let all_arr = [...food,...flowers,...fruits];//morden js system,ata k spred operator system bola hoy.
+// let all_arr = food.join(',');//convert array to string.
+
+// let info = 'my name is faisal , I love javascript';
+// let all_arr = info.split(' ');//convert string to array.
+
+//console.log(all_arr);
+
+// let color = ['Blue','Red','Green','Yellow',];
+// color.push('white');
+// color.pop();
+// color.pop();
+// color.unshift('Black');
+// color.shift();
+// color.shift();
+// console.log(color);
+
+// let food1 =['begun','potol','lao','derosh','jinga','kumra','sosa']
+//let food2 = food1.splice(3,2);
+// let food2 = food1.slice(3,5);
+
+// console.log(food1);
+// console.log(food2);
