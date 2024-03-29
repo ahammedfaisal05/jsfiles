9420e3ff9c95d3b0c8d439541e9b7b1083543213
ahammedfaisal05 <faisalahammed05@gmail.com>
2024-03-29 20:47:57 +0600

    Added multidimantionaarray

---
+++ multidimantionalarray.js
@@ -0,0 +1,42 @@
+/*const phonebook = [
+    ['Faisal','01444444433','charpathalia','faisal@gmail.com'],
+    ['tarek','01444444433','charpathalia','tarek@gmail.com'],
+    ['Shahadat','01443747754','bhaberchar','shahadat@gmail.com'],
+    ['Robin','01654744788','dhaka','robin@gmail.com'],
+    ['tasin','016666666','gazaria','tasin@gmail.com'],
+    ['Fahim','01656867856','Gazipur','fahim@gmail.com'],
+    ['karim','016568556677','dhaka','karim@gmail.com'],
+];
+
+phonebook.sort().reverse().forEach((data, index)=>{
+
+    if(data[2] === 'charpathalia' || data[2] === 'dhaka')
+    console.log(`
+        Id = ${index + 1}
+        Name = ${data[0]}
+        Cell = ${data[1]}
+        Location = ${data[2]}
+        Gmail = ${data[3]}
+    `);
+});*/
+
+/*
+const tex = [
+    ['Faisal','01444444433','charpathalia','1200'],
+    ['tarek','01444444433','charpathalia','566'],
+    ['Shahadat','01443747754','bhaberchar','600'],
+    ['Robin','01654744788','dhaka','400'],
+    ['tasin','016666666','gazaria','700'],
+    ['Fahim','01656867856','Gazipur','100'],
+    ['karim','016568556677','dhaka','300'],
+    ['karim','016568556678','dhaka','800'],
+];
+let total_amount = 0;
+tex.sort().map((data, i)=>{
+    console.log(`Name: ${data[0]} : Amount ${data[3]}`);
+    total_amount += Number(data[3]);
+});
+console.log(`--------------------------------
+Total Amount = ${total_amount}
+`);
+*/
