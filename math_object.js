83ecc295b06f740bb24fe1e333c4962b70fe6d8f
ahammedfaisal05 <faisalahammed05@gmail.com>
2024-04-08 13:08:02 +0600

    added math object

---
+++ math_object.js
@@ -0,0 +1,23 @@
+// Math Object
+let age = -30;
+console.log(Math.abs(age));/*'Math' hocche js er ekta buildin object,'abs' is a method in js, abs method always - value k + kore,
+                        Ar Math.abs() hocche js er buildin function. */
+
+let amount = 30.50;
+console.log(Math.round(amount));/*'round' method e .50 hole next value dibe, r .49 hole (.)er ager value dibe.
+                                ex : 30.50 = 31 ; 30.49 = 30 */
+
+let amountt = 50.23;
+console.log(Math.floor(amountt));/* 'floor' mathod e (.) er por joto sonkhai thakok. 'floor' method 
+                        (.)er ager value dibe. ex: 40.67 = 40 ; 50.23 = 50;*/
+
+
+let amounttt = 40.67;
+console.log(Math.ceil(amounttt)); /* 'ceil' mathod e (.) er por joto sonkhai thakok. 'ceil' method 
+                        real number er porer value dibe. ex: 40.67 = 41 ; 50.23 = 51;*/
+
+
+console.log(Math.max(12,34,55,677,23,1111));
+console.log(Math.sqrt(9));//'sqrt' method square root kore.
+console.log(Math.pow(9,2));//'pow' method power use kora hoy.
+console.log(Math.random()*1000);
