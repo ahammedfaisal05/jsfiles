72a9089a1f31893e1c68608e83df3cfe60723fcd
Ahammed Faisal <137277285+ahammedfaisal05@users.noreply.github.com>
2024-11-12 20:07:18 +0600

    Objective in js

---
+++ test.js
@@ -0,0 +1,16 @@
+
+// OBJECTIVE practics
+const user2 = {
+    lang_name : 'javascript',
+    type : 'programming',
+    user_address : {
+        name : 'Faisal',
+        age : 26,
+        goallife: {
+            passion: 'S engineer',
+            born: 1998
+        }
+    }
+}
+
+user2.color = 'blue';
