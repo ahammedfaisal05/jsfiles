7c9e3890eb393339479234d3e222f92dac4a6974
Ahammed Faisal <137277285+ahammedfaisal05@users.noreply.github.com>
2024-11-30 10:19:22 +0600

    while loop practics

---
+++ test.js
@@ -0,0 +1,19 @@
+
+console.log('program started');
+
+// debugger
+// let i = 1
+// while(i <= 40){
+//     console.log(i);
+//     i++
+// }
+
+//----------------------
+
+const month = ['jan','feb','mar','apr','may','jun','jul','aug','sep','oct','nob','decm'];
+let i = 0
+while(i < 12){
+    console.log(`${i + 1}. ${month[i]}`);
+    i++
+}
+console.log('progrm ended');
