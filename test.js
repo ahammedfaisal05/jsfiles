995bbf881b20f51b22d7676c4042f2e8fe635656
Ahammed Faisal <137277285+ahammedfaisal05@users.noreply.github.com>
2024-11-30 23:17:19 +0600

    Add files via upload

---
+++ test.js
@@ -0,0 +1,24 @@
+
+console.log('program started');
+
+// debugger
+// let i = 1
+// while(i <= 40){
+//     console.log(i);
+//     i++
+// }
+
+//----------------------
+
+// const month = ['jan','feb','mar','apr','may','jun','jul','aug','sep','oct','nob','decm'];
+// let i = 0
+// while(i < 12){
+//     console.log(`${i + 1}. ${month[i]}`);
+//     i++
+// }
+
+
+for(i = 0 ; i < 100; i++){
+    console.log(i+1,"sorry");
+}
+console.log('progrm ended');
